6d838b99e4c3511eec0f108b83509a9866b22751
daithi3721 <m.daithi3721@gmail.com>
2015-12-17 10:17:51 +0000

    user-profile-final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+ before_action :correct_user, only: [:edit, :update]
+ 
   def show
     @user = User.find(params[:id])
   end
@@ -37,5 +38,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :age, :password, :password_confirmation)
   end
-
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url)unless @user == current_user
+  end
 end
